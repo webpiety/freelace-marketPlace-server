b80576c57e92761384508dd540c56ddda90a7c15
webpiety <mohammad.bhossain@gmail.com>
2025-11-09 20:43:06 +0600

    Connect to mongodb and create env file

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 2173;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ng2yszq.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server id running fine");
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connect to mongodb");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
